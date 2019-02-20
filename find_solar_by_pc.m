0928ce52f7e7e80b82aa71d75b46a6b4c3f87937
Lachlan Andrew <lachlan.andrew@monash.edu>
2019-02-20 15:03:51 +1100

    find_solar_by_pc.m: Remove test for solar_users being empy.  How did that work??

--- find_solar_by_pc.m
+++ find_solar_by_pc.m
@@ -5,11 +5,11 @@ function [s] = find_solar_by_pc (s, meta)
   tic;
   for k = 1:length(meta.pclist)
     % Index into solar_users  of those in post code k
-    if ~isempty(s.solar_users)
-        u = 1;
-    else
+%    if ~isempty(s.solar_users)
+%        u = 1;
+%    else
         u = find(meta.postcode(s.solar_users) == meta.pclist(k));
-    end
+%    end
     
     % May have problems if some have very small cap factor
     % Only consider those that can generate >100W now
